f10adb9681b8d1ffd151d5f080c83466e43effed
klrc <1440698245@qq.com>
2021-09-17 11:21:38 +0800

    add arg parser

--- src/forge/train.py
+++ src/forge/train.py
@@ -1,8 +1,16 @@
 import torch
 
-def train(model, options):
+def project_path(options):
+    # Directories
+    save_dir = Path(options.save_dir)
+    w = save_dir / 'weights'  # weights dir
+    w.mkdir(parents=True, exist_ok=True)  # make dir
+    return w
+
+
+def train(model, options, device):
     w = project_path(options)
-    model, start_epoch = try_load_ckpt(model, options)
+    model, start_epoch = try_load_ckpt(model, options, device)
     optimizer = create_optimizer(model, options)
     train_loader, val_loader = create_dataloader(options)
     max_epoch = 0  # get max_epoch from options
@@ -12,7 +20,20 @@ def train(model, options):
         pbar = enumerate(train_loader)
         for i, (imgs, targets, paths, _) in pbar:
            loss_items = train_batch(model, optimizer, imgs, targets, paths) 
-        results = val(model, val_loader)
+        results = val.run(model, val_loader)
         on_train_val_end()
     torch.cuda.empty_cache()
     return
+
+def main(opt):
+    device = select_device(opt)
+    model = create_model(opt, device)
+    return train(model, opt, device)
+
+
+def run(**kwargs):
+    # Usage: import train; train.run(data='coco128.yaml', imgsz=320, weights='yolov5m.pt')
+    opt = parse_opt(True)
+    for k, v in kwargs.items():
+        setattr(opt, k, v)
+    return main(opt)
