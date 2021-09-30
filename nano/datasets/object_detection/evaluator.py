caf43a975f727cad1c1fb781e37c0ac772488f9f
klrc <1440698245@qq.com>
2021-09-30 14:25:56 +0800

    add yolov5 style trainer

--- nano/datasets/object_detection/evaluator.py
+++ nano/datasets/object_detection/evaluator.py
@@ -29,92 +29,85 @@ def process_batch(predictions, labels, iouv):
     return correct
 
 
-class CallmAP(nn.Module):
-    def __init__(self, val_loader, device, names, conf_thres=0.25, iou_thres=0.45):
-        super().__init__()
-        self.val_loader = val_loader
-        self.names = {i: n for i, n in enumerate(names)}
-        self.conf_thres = conf_thres  # confidence threshold
-        self.iou_thres = iou_thres    # NMS IOU threshold
-        self.device = device
+def val(model, val_loader, names, device, conf_thres, iou_thres):
+    p, r, f1, mp, mr, map50, map = 0, 0, 0, 0, 0, 0, 0
+    stats, ap, ap_class = [], [], []
+    names = {i: n for i, n in enumerate(names)}
+    nc = len(names)
 
-    def forward(self, model):
-        val_loader = self.val_loader
-        p, r, f1, mp, mr, map50, map = 0, 0, 0, 0, 0, 0, 0
-        stats, ap, ap_class = [], [], []
-        names = self.names
-        device = self.device
-        nc = len(names)
+    # Start evaluation
+    model.eval().to(device)
+    for batch_i, (imgs, targets, paths, shapes) in enumerate(val_loader):
+        imgs = imgs.to(device)
+        targets = targets.to(device)
+        imgs = imgs.float() / 255.0  # uint8 to float32, 0-255 to 0.0-1.0
+        out = model.inference(imgs)  # inference and training outputs
+        iouv = torch.linspace(0.5, 0.95, 10).to(device)  # iou vector for mAP@0.5:0.95
+        niou = iouv.numel()
+        _, _, height, width = imgs.shape  # batch size, channels, height, width
 
-        # Start evaluation
-        model.to(device)
-        model.eval()
+        # Run NMS
+        targets[:, 2:] *= torch.Tensor([width, height, width, height]).to(device)  # to pixels
+        lb = []  # for autolabelling
+        out = non_max_suppression(out, conf_thres, iou_thres, labels=lb, multi_label=True, agnostic=False)
 
-        for batch_i, (imgs, targets, paths, shapes) in enumerate(val_loader):
-            imgs = imgs.to(device)
-            targets = targets.to(device)
-            imgs = imgs.float() / 255.0  # uint8 to float32, 0-255 to 0.0-1.0
-            out = model.inference(imgs)  # inference and training outputs
-            iouv = torch.linspace(0.5, 0.95, 10).to(device)  # iou vector for mAP@0.5:0.95
-            niou = iouv.numel()
-            _, _, height, width = imgs.shape  # batch size, channels, height, width
+        # Statistics per image
+        for si, pred in enumerate(out):
+            labels = targets[targets[:, 0] == si, 1:]
+            nl = len(labels)
+            tcls = labels[:, 0].tolist() if nl else []  # target class
+            shape = shapes[si][0]
 
-            # Run NMS
-            targets[:, 2:] *= torch.Tensor([width, height, width, height]).to(device)  # to pixels
-            lb = []  # for autolabelling
-            out = non_max_suppression(out, self.conf_thres, self.iou_thres, labels=lb, multi_label=True, agnostic=False)
-
-            # Statistics per image
-            for si, pred in enumerate(out):
-                labels = targets[targets[:, 0] == si, 1:]
-                nl = len(labels)
-                tcls = labels[:, 0].tolist() if nl else []  # target class
-                shape = shapes[si][0]
-
-                # Predictions
-                if len(pred) == 0:
-                    if nl:
-                        stats.append((torch.zeros(0, niou, dtype=torch.bool), torch.Tensor(), torch.Tensor(), tcls))
-                    continue
-                predn = pred.clone()
-                scale_coords(imgs[si].shape[1:], predn[:, :4], shape, shapes[si][1])  # native-space pred
-
-                # Evaluate
+            # Predictions
+            if len(pred) == 0:
                 if nl:
-                    tbox = xywh2xyxy(labels[:, 1:5])  # target boxes
-                    scale_coords(imgs[si].shape[1:], tbox, shape, shapes[si][1])  # native-space labels
-                    labelsn = torch.cat((labels[:, 0:1], tbox), 1)  # native-space labels
-                    correct = process_batch(predn, labelsn, iouv)
-                else:
-                    correct = torch.zeros(pred.shape[0], niou, dtype=torch.bool)
-                stats.append((correct.cpu(), pred[:, 4].cpu(), pred[:, 5].cpu(), tcls))  # (correct, conf, pcls, tcls)
+                    stats.append((torch.zeros(0, niou, dtype=torch.bool), torch.Tensor(), torch.Tensor(), tcls))
+                continue
+            predn = pred.clone()
+            scale_coords(imgs[si].shape[1:], predn[:, :4], shape, shapes[si][1])  # native-space pred
 
-            # End batch -------------------
+            # Evaluate
+            if nl:
+                tbox = xywh2xyxy(labels[:, 1:5])  # target boxes
+                scale_coords(imgs[si].shape[1:], tbox, shape, shapes[si][1])  # native-space labels
+                labelsn = torch.cat((labels[:, 0:1], tbox), 1)  # native-space labels
+                correct = process_batch(predn, labelsn, iouv)
+            else:
+                correct = torch.zeros(pred.shape[0], niou, dtype=torch.bool)
+            stats.append((correct.cpu(), pred[:, 4].cpu(), pred[:, 5].cpu(), tcls))  # (correct, conf, pcls, tcls)
 
-        # Compute statistics
-        stats = [np.concatenate(x, 0) for x in zip(*stats)]  # to numpy
-        if len(stats) and stats[0].any():
-            p, r, ap, f1, ap_class = ap_per_class(*stats, names=names)
-            ap50, ap = ap[:, 0], ap.mean(1)  # AP@0.5, AP@0.5:0.95
-            mp, mr, map50, map = p.mean(), r.mean(), ap50.mean(), ap.mean()
-            nt = np.bincount(stats[3].astype(np.int64), minlength=nc)  # number of targets per class
-        else:
-            nt = torch.zeros(1)
-        fi = fitness(np.array([mp, mr, map50, map]).reshape(1, -1))  # weighted combination of [P, R, mAP@.5, mAP@.5-.95]
-        metrics = {
-            'precision': mp,
-            'recall': mr,
-            'mAP:.5': map50,
-            'mAP': map,
-        }
-        if nc > 1 and len(stats):
-            for i, c in enumerate(ap_class):
-                metrics[f'num_targets-{names[c]}'] = nt[c]
-                metrics[f'precision-{names[c]}'] = p[i].item(),
-                metrics[f'recall-{names[c]}'] = r[i].item(),
-                metrics[f'mAP:.5-{names[c]}'] = ap50[i].item(),
-                metrics[f'mAP-{names[c]}'] = ap[i].item(),
-        for k, v in metrics.items():
-            if type(v) is tuple:
-                metrics[k] = v[0]
-        return fi, metrics
+        # End batch -------------------
+
+    # Compute statistics
+    stats = [np.concatenate(x, 0) for x in zip(*stats)]  # to numpy
+    if len(stats) and stats[0].any():
+        p, r, ap, f1, ap_class = ap_per_class(*stats, names=names)
+        ap50, ap = ap[:, 0], ap.mean(1)  # AP@0.5, AP@0.5:0.95
+        mp, mr, map50, map = p.mean(), r.mean(), ap50.mean(), ap.mean()
+        nt = np.bincount(stats[3].astype(np.int64), minlength=nc)  # number of targets per class
+    else:
+        nt = torch.zeros(1)
+    # mAPs
+    maps = np.zeros(nc) + map
+    for i, c in enumerate(ap_class):
+        maps[c] = ap[i]
+    # fitness
+    fi = fitness(np.array([mp, mr, map50, map]).reshape(1, -1))  # weighted combination of [P, R, mAP@.5, mAP@.5-.95]
+    metrics = {
+        'precision': mp,
+        'recall': mr,
+        'mAP:.5': map50,
+        'mAP': map,
+    }
+    # precision, recall, mAP:.5, mAP
+    if nc > 1 and len(stats):
+        for i, c in enumerate(ap_class):
+            metrics[f'num_targets-{names[c]}'] = nt[c]
+            metrics[f'precision-{names[c]}'] = p[i].item(),
+            metrics[f'recall-{names[c]}'] = r[i].item(),
+            metrics[f'mAP:.5-{names[c]}'] = ap50[i].item(),
+            metrics[f'mAP-{names[c]}'] = ap[i].item(),
+    for k, v in metrics.items():
+        if type(v) is tuple:
+            metrics[k] = v[0]
+    return fi, maps, metrics
