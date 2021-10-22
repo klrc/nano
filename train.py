00e6cb99597ad7ac3d18553b7c9e65aea73d41cb
klrc <1440698245@qq.com>
2021-10-22 12:54:35 +0800

    add cos-s/n/f datasets

--- train.py
+++ train.py
@@ -1,7 +1,7 @@
 
 import nano
 
-model = nano.models._test_sample_model()
-# trainer = nano.datasets.detection.some_trainer()
+model = nano.models.yolov5_cspm()
+trainer = nano.datasets.object_detection.trainer
 
 # trainer.run(model, data='data/coco-s+indoor.yaml', hyp='data/hyps/hyp.finetune.yaml', epochs=1000, imgsz=416)
