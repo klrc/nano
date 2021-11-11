4751f621343c0857a95248e13284dd99d90f9233
klrc <1440698245@qq.com>
2021-11-11 10:46:57 +0800

    .

--- nano/models/yolox_cspm_depthwise_test/__init__.py
+++
@@ -1,1 +0,0 @@
-from .model import yolox_cspm_depthwise_test
---
+++ realtime_test.py
@@ -0,0 +1,161 @@
+import cv2
+import torch
+import torchvision.transforms as T
+import nano
+from multiprocessing import Queue, Process
+
+
+def detection_iou(box1, box2):
+    area_a = (box1[2] - box1[0]) * (box1[3] - box1[1])
+    area_b = (box2[2] - box2[0]) * (box2[3] - box2[1])
+    w = min(box1[2], box2[2]) - max(box1[0], box2[0])
+    h = min(box1[3], box2[3]) - max(box1[1], box2[1])
+    if w <= 0 or h <= 0:
+        return 0
+    area_c = w * h
+    return area_c / (area_a + area_b - area_c)
+
+
+def detection_nms(preds):
+    """Runs Non-Maximum Suppression (NMS) on inference results
+    Returns:
+         list of detections, on (n,6) tensor per image [xyxy, conf, cls]
+    """
+
+    # Settings
+    max_nms = 30000  # maximum number of boxes into torchvision.ops.nms()
+    conf_thres = 0.25  # confidence threshold
+    iou_thres = 0.45  # iou threshold
+    results = []
+
+    # Reformat data
+    for pred in preds:
+        obj_conf = pred[4]
+        cls_conf, cls_id = pred[5:].max(dim=0, keepdim=True)  # Confidence calculation
+        conf = obj_conf * cls_conf
+        if conf < conf_thres:  # Select candidates
+            continue
+        x1 = pred[0] - pred[2] / 2.0  # Box (center x, center y, width, height) to (x1, y1, x2, y2)
+        y1 = pred[1] - pred[3] / 2.0
+        x2 = pred[0] + pred[2] / 2.0
+        y2 = pred[1] + pred[3] / 2.0
+        results.append([x1, y1, x2, y2, conf, cls_id])  # Detections matrix nx6
+
+    # Check results
+    n = len(results)  # number of boxes
+    if n == 0:  # no boxes
+        return results
+    results = sorted(results, key=lambda x: x[4], reverse=True)  # sort by confidence
+    if n > max_nms:  # excess boxes
+        results = results[:max_nms]
+        n = max_nms
+
+    # Batched NMS
+    targets = [i for i in range(n)]
+    keeped_targets = []
+    while len(targets) > 0:
+        t = targets.pop(0)
+        keeped_targets.append(t)
+        for i, x in enumerate(targets):
+            if detection_iou(results[x], results[t]) > iou_thres:
+                targets[i] = -1  # abandon
+        targets = [i for i in targets if i != -1]
+    return [results[i] for i in keeped_targets]
+
+
+def detection_post_process(
+    preds,
+    anchors=[10.875, 14.921875, 31.1875, 53.28125, 143.0, 157.5],
+    strides=[8, 16, 32],
+    num_classes=3,
+):
+    # reshape outputs
+    results = []
+    anchors = torch.tensor(anchors).view(len(strides), 1, 1, 1, 2)
+    for i, pred in enumerate(preds):  # predictions with different scales
+        y = pred.sigmoid()
+        y = y.permute(0, 2, 3, 1)
+        bs, _, ny, nx = pred.shape
+        yv, xv = torch.meshgrid([torch.arange(ny), torch.arange(nx)])
+        grid = torch.stack((xv, yv), 2).view((1, ny, nx, 2)).float()
+        y[..., 0:2] = (y[..., 0:2] * 2.0 - 0.5 + grid) * strides[i]  # x, y
+        y[..., 2:4] = (y[..., 2:4] * 2) ** 2 * anchors[i]  # w, h
+        results.append(y.view(bs, -1, num_classes + 5))
+    results = torch.cat(results, 1)
+    results = results.squeeze(0)
+    results = detection_nms(results)
+    return results
+
+
+def detection(capture_queue, bbox_queue):
+    model = nano.models.yolox_cspm_depthwise_test(num_classes=3)
+    model.load_state_dict(torch.load("runs/train/exp112/weights/last.pt", map_location="cpu")["state_dict"])
+    model.dsp().cuda()
+    transforms = T.Compose([T.ToPILImage(), T.Resize((224, 416)), T.ToTensor()])
+
+    while True:
+        if not capture_queue.empty():
+            frame = capture_queue.get()
+            # process image
+            x = transforms(frame)
+            results = model(x.unsqueeze(0).cuda())
+            results = [x.cpu() for x in results]
+            results = detection_post_process(results)
+            results = [[float(xi) for xi in x] for x in results]
+            bbox_queue.put(results)
+
+
+def cv2_draw_bbox(frame, x):
+    x1, y1, x2, y2, conf, cls_id = x
+    x1 = int(x1 / 416 * width)
+    x2 = int(x2 / 416 * width)
+    y1 = int(y1 / 224 * height)
+    y2 = int(y2 / 224 * height)
+    cls_map = ["person", "two-wheeler", "car"]
+    label = f"detected: {cls_map[int(cls_id)]} {conf:.2%}"
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    font_scale = 0.5
+    font_thickness = 1
+    cv2.rectangle(frame, (x1, y1), (x2, y2), (75, 0, 130), 1, cv2.LINE_AA, 0)
+    text_size, _ = cv2.getTextSize(label, font, font_scale, font_thickness)
+    text_w, text_h = text_size
+    cv2.rectangle(frame, (x1, y1), (x1 + text_w + 2, y1 + text_h + 2), (75, 0, 130), -1)
+    cv2.putText(frame, label, (x1, y1 + text_h), font, font_scale, (255, 255, 255), font_thickness)
+
+
+if __name__ == "__main__":
+    capture = cv2.VideoCapture(0)  # VideoCapture 读取本地视频和打开摄像头
+    height = capture.get(cv2.CAP_PROP_FRAME_HEIGHT)  # 计算视频的高
+    width = capture.get(cv2.CAP_PROP_FRAME_WIDTH) + 125 * 2  # 计算视频的宽
+    capture_queue = Queue(maxsize=1)
+    result_queue = Queue(maxsize=64)
+    bbox_set = []
+
+    try:
+        # inference process --------------
+        proc_1 = Process(target=detection, args=[capture_queue, result_queue])
+        proc_1.daemon = True
+        proc_1.start()
+
+        # # capture process ----------------
+        while True:
+            ret, frame = capture.read()
+            frame = cv2.copyMakeBorder(frame, 0, 0, 125, 125, cv2.BORDER_CONSTANT, value=(0, 0, 0))
+            if ret is False:
+                break
+            frame = cv2.flip(frame, 1)  # cv2.flip 图像翻转
+            if capture_queue.empty():
+                # print("put frame", capture_queue.qsize())
+                capture_queue.put(frame)
+            if not result_queue.empty():
+                bbox_set = result_queue.get()
+            if len(bbox_set) > 0:
+                for x in bbox_set:
+                    cv2_draw_bbox(frame, x)
+            cv2.imshow("frame", frame)
+            if cv2.waitKey(10) == 27:
+                break
+    except Exception as e:
+        capture.release()
+        cv2.destroyAllWindows()
+        raise e
