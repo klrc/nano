f12a2a5fc9bbb1cd0a589e0f5f3efc2b549ae226
klrc <1440698245@qq.com>
2021-11-16 14:29:49 +0800

    。

--- export.py
+++ export.py
@@ -2,11 +2,9 @@ import nano
 from nano._utils import freeze
 import torch
 
-# model = nano.models.yolox_esmk_shrink(num_classes=3).dsp()
+model = nano.models.yolox_esmk_shrink(num_classes=3).dsp()
 # model.load_state_dict(torch.load("runs/train/exp130/weights/best.pt", map_location="cpu")["state_dict"])
-# "release/yolox_esmk/yolox_esmk_shrink.onnx"
-model = ...
-onnx_path = ...
+onnx_path = "release/yolox_esmk_v2/yolox_esmk_shrink.onnx"
 output_names = ["output_1", "output_2", "output_3"]
 
 freeze(
